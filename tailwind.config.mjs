5b286d9151d2a5afbbcb0c68654431c416a945bf
Pablo Henrique <pablohmpchamber@gmail.com>
2024-06-24 11:22:30 -0300

    azul da maxtec

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+
+			colors: {
+				'maxtec': '#325082',
+				'maxtec-light': '#325082',
+				'maxtec-dark': '#325082',
+				'maxtec-darker': '#325082',
+				'maxtec-darkest': '#325082',
+				'maxtec-lighter': '#325082',
+				'maxtec-lightest': '#325082',
+			}
+		},
 	},
 	plugins: [
 
